da8f0109ac7ff54fd906c178b0b13a6c771576ef
Adalberto R. Ribeiro <beto1821@uol.com.br>
2024-03-10 15:59:35 -0300

    Css puro setup

--- src/App.js
+++ src/App.js
@@ -19,33 +19,42 @@ export default class App extends Component {
     const { fraseOriginal, fraseCodificada, fraseDecodificada } = this.state;
     return (
     <body>
-      <header >Codificar e Decodificar Frases</header>  
-      <div className="App">
-        <textarea
-          value={fraseOriginal}
-          onChange={(e) => this.setState({ fraseOriginal: e.target.value })}
-          rows={4}
-          cols={50}
-          placeholder="Digite a frase..."
-        />
-        <ButtonCod
-          fraseOriginal={fraseOriginal}
-          setFraseCodificada={(novaFraseCodificada) => this.setState({ fraseCodificada: novaFraseCodificada })}
-        />
-        <ButtonDecod
-          fraseOriginal={fraseOriginal}
-          setFraseDecodificada={(novaFraseDecodificada) => this.setState({ fraseDecodificada: novaFraseDecodificada })}
-        />
-        <div>
-          <h3>Frase </h3>
-          <p>{fraseCodificada}</p>
-          <p>{fraseDecodificada}</p>
-        </div>
-        <ButtonCopy
-          fraseCodificada={fraseCodificada}
-        />
-        <button onClick={this.handleRefresh}>Reiniciar</button>
-      </div>
+      <header >Codificar e Decodificar Frases</header>
+        <main className='App'>
+          <div className="Input">
+            <textarea className='TextArea'
+              value={fraseOriginal}
+              onChange={(e) => this.setState({ fraseOriginal: e.target.value })}
+              rows={4}
+              cols={50}
+              placeholder="Digite a frase..."
+            />
+  
+            <div className='botaoCod'>
+              <ButtonCod
+                fraseOriginal={fraseOriginal}
+                setFraseCodificada={(novaFraseCodificada) => this.setState({ fraseCodificada: novaFraseCodificada })}
+              />
+              <ButtonDecod
+                fraseOriginal={fraseOriginal}
+                setFraseDecodificada={(novaFraseDecodificada) => this.setState({ fraseDecodificada: novaFraseDecodificada })}
+              />
+            </div>
+          </div> 
+          <div className='Output'>
+              <h3>Frase </h3>
+              <p>{fraseCodificada}</p>
+              <p>{fraseDecodificada}</p>
+          
+            <ButtonCopy
+              fraseCodificada={fraseCodificada}
+            />
+          </div>
+        </main>  
+        <button className='reset' onClick={this.handleRefresh}>Reiniciar</button>
+        <footer className='footer'>
+            Made in React with love
+        </footer>
     </body>
     );
   }
