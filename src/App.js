203006331c1c08cb90613fb2da4e906c951d7020
Adalberto R. Ribeiro <beto1821@uol.com.br>
2024-03-11 21:20:00 -0300

    feat footer icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ButtonCod from './components/ButtonCod';
 import ButtonDecod from './components/ButtonDecod';
 import ButtonCopy from './components/ButtonCopy';
 import ImgBusca from './img/busca.svg';
+import github from './img/icons8-github-26.png';
+import linkedin from './img/icons8-linkedin-26.png';
 import './App.css';
 
 export default class App extends Component {
@@ -60,9 +62,14 @@ export default class App extends Component {
         <div className="buttonCopy">
          <button className='btnCopy' onClick={this.handleRefresh}>Reiniciar</button>
         </div>
-        <footer className='footer'>
-            Made in React with love
-            <div id="heart"></div>
+        <footer>
+            <p>Made in React with love</p>
+            <a href='https://github.com/Beto1821' >
+              <img src={github} alt='github/Beto1821' />
+            </a>
+            <a href='https://www.linkedin.com/in/adalberto-ramos-ribeiro-344092107/' >
+              <img src={linkedin} alt='linkedin' />
+            </a>
         </footer>
     </body>
     );
